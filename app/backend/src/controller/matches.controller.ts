69f0909b855fb0d54e7aa59a19a26daad4b871f8
Luiz de Freitas <luizdefreitas10@gmail.com>
2022-12-12 08:52:41 -0300

    Reqs 20 e 21, implementacoes da rota matches

--- app/backend/src/controller/matches.controller.ts
+++ app/backend/src/controller/matches.controller.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from 'express';
 import * as matchesService from '../database/service/matches.service';
 
-const getAll = async (_req: Request, res: Response) => {
-  const { status, message } = await matchesService.default.getAll();
+const getAll = async (req: Request, res: Response) => {
+  const { inProgress } = req.query;
+  const { status, message } = !inProgress
+    ? await matchesService.default.getAll()
+    : await matchesService.default.filterByProgress(JSON.parse(inProgress as string));
   if (status) return res.status(status).json({ message });
   res.status(200).json(message);
 };
+
+// const filterByProgress = async (req: Request, res: Response) => {
+//   const { status, message } = await matchesService.default.filterByProgress(inProgress);
+// };
 
 export default { getAll };
--- app/backend/src/database/service/matches.service.ts
+++ app/backend/src/database/service/matches.service.ts
@@ -13,4 +13,18 @@ const getAll = async () : Promise<{ status: number | null, message: MatchModel[]
   return { status: null, message: match };
 };
 
-export default { getAll };
+const filterByProgress = async (query: boolean)
+:Promise<{ status: number | null, message: MatchModel[] }> => {
+  const filtered = await MatchModel.findAll({
+    where: { inProgress: query },
+    include: [{
+      model: Team, as: 'teamHome',
+    },
+    {
+      model: Team, as: 'teamAway',
+    }],
+  });
+  return { status: null, message: filtered };
+};
+
+export default { getAll, filterByProgress };
