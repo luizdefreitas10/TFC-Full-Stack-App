fda5a48ff12fd758672f6eff3b780d16f55b39f5
Luiz de Freitas <luizdefreitas10@gmail.com>
2022-12-12 09:00:38 -0300

    tentando corrigir lint

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -11,7 +11,7 @@ class App {
     this.config();
 
     // Não remover essa rota
-    this.app.get('/', (req, res) => res.json({ ok: true }));
+    this.app.get('/', (_req, res) => res.json({ ok: true }));
     this.app.use('/login', userRouter);
     this.app.use('/teams', teamsRouter);
     this.app.use('/matches', matchesRouter);
